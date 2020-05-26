b839affa81f083f8a8114bbb12b01b72a50e04f5
Mathias Maillard <mathias.maillard11@gmail.com>
2020-05-26 21:27:44 +0200

    bad smell: AddProduct with big A, refactor: renamed addProduct

--- src/test/java/ch/heigvd/gen2019/OrdersWriterTest.java
+++ src/test/java/ch/heigvd/gen2019/OrdersWriterTest.java
@@ -3,10 +3,11 @@ package ch.heigvd.gen2019;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class OrdersWriterTest {
     ArrayList<Order> orders = new ArrayList<>();
@@ -39,7 +40,7 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProduct() {
-        order111.AddProduct(new Product("Shirt", Color.BLUE, Size.M, 2.99, "TWD"));
+        order111.addProduct(new Product("Shirt", Color.BLUE, Size.M, 2.99, "TWD"));
 
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContentsJSON());
@@ -47,7 +48,7 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProductNoSize() {
-        order111.AddProduct(new Product("Pot", Color.RED, Size.NONE, 16.50, "SGD"));
+        order111.addProduct(new Product("Pot", Color.RED, Size.NONE, 16.50, "SGD"));
 
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContentsJSON());
