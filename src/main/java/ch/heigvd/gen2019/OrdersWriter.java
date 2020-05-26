f2b0dbf2fc054993ca4f93e640a4b1fb4e4283dd
Mathias Maillard <mathias.maillard11@gmail.com>
2020-05-26 17:47:46 +0200

    bad smell: getContents() too long, refactor: extract method

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -11,23 +11,7 @@ public class OrdersWriter {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-
-                order.getProduct(j).getContent(sb);
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
+            getOrderContent(sb, i);
         }
 
         if (orders.getOrdersCount() > 0) {
@@ -35,6 +19,26 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
+    }
+
+    private void getOrderContent(StringBuffer sb, int i) {
+        Order order = orders.getOrder(i);
+        sb.append("{");
+        sb.append("\"id\": ");
+        sb.append(order.getOrderId());
+        sb.append(", ");
+        sb.append("\"products\": [");
+        for (int j = 0; j < order.getProductsCount(); j++) {
+
+            order.getProduct(j).getContent(sb);
+        }
+
+        if (order.getProductsCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        sb.append("]");
+        sb.append("}, ");
     }
 
 }
