248f493e13ed9bf1b4c9683d7ffb5962230026c1
Mathias Maillard <mathias.maillard11@gmail.com>
2020-05-26 21:22:53 +0200

    bad smell: useless methods for Order, refactor: inline methods

--- src/main/java/ch/heigvd/gen2019/Order.java
+++ src/main/java/ch/heigvd/gen2019/Order.java
@@ -11,18 +11,6 @@ public class Order {
         this.id = id;
     }
 
-    public int getOrderId() {
-        return id;
-    }
-
-    public int getProductsCount() {
-        return products.size();
-    }
-
-    public Product getProduct(int j) {
-        return products.get(j);
-    }
-
     public void AddProduct(Product product) {
         products.add(product);
     }
@@ -31,15 +19,15 @@ public class Order {
         StringBuffer sb = new StringBuffer();
         sb.append("{");
         sb.append("\"id\": ");
-        sb.append(getOrderId());
+        sb.append(id);
         sb.append(", ");
         sb.append("\"products\": [");
-        for (int j = 0; j < getProductsCount(); j++) {
+        for (int j = 0; j < products.size(); j++) {
 
-            sb.append(getProduct(j).getProductJSON());
+            sb.append(products.get(j).getProductJSON());
         }
 
-        if (getProductsCount() > 0) {
+        if (products.size() > 0) {
             sb.delete(sb.length() - 2, sb.length());
         }
 
